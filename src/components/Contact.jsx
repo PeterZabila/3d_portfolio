d31f7d75ca47cd5ebf885da02bcf7dada8ab45a8
PeterZabila <petrozabila@gmail.com>
2023-04-26 17:36:34 +0300

    build

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -50,19 +50,6 @@ emailjs.send(
   'bWMquDmtZwdr5JsG7'
 )
 
-    // emailjs
-    //   .send(
-    //     import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-    //     import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
-    //     {
-    //       from_name: form.name,
-    //       to_name: 'Petro',
-    //       from_email: form.email,
-    //       to_email: "petrozabila@gmail.com",
-    //       message: form.message,
-    //     },
-    //     import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
-    //   )
       .then(
         () => {
           setLoading(false);
